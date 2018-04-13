c4907084dd19aed1e6de0e08d68e703fc3119768
M4rkFlor <M4rkFlor@users.noreply.github.com>
2018-04-12 21:02:22 -0700

    Enviroment port

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require("./routes");
 var clientSessions = require("client-sessions");
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use('/', express.static('./'));
 app.use('/js', express.static('./public/js'));
 app.use('/images', express.static('./public/images'));
 app.use(clientSessions({
-  secret: 'yup this a secret' // CHANGE THIS!
+    secret: 'yup this a secret' // CHANGE THIS!
 }));
 app.use(routes);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
 });
-
